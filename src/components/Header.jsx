545f11f2c11794fb6d445d81d9ef37eefdbe5145
gunjan <gunjandora595@gmail.com>
2023-02-13 15:23:29 +0530

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dolorum
+        quis voluptatibus dolor autem, exercitationem recusandae quisquam
+        repellendus, maiores asperiores vero? Repudiandae, nisi omnis? Odio
+        labore facilis nam. Alias, minus!
+      </p>
+    </div>
+  );
 };
 
 export default Header;
